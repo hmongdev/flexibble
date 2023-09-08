25cb00d8dbffdf41c314a51b1a13122f5b64fc02
Avery Yang <averyyang2674@gmail.com>
2023-09-08 14:53:01 -0500

    set up grafbase config

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,5 +1,29 @@
 import { g, auth, config } from '@grafbase/sdk'
 
+// setting this up will allow us to query and mutate on grafbase
+
+// create User model
+const User = g.model('User', {
+  // with the following properties
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.relation(() => Project).list().optional()
+})
+
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+})
+
 export default config({
   schema: g
 })
