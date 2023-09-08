272eeaee79ddc7ff66d6401e974ef37c0b66f32d
Avery Yang <averyyang2674@gmail.com>
2023-09-08 14:44:53 -0500

    create grafbase with config

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 const Home = () => {
   return (
-    <div></div>
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>LoadMore</h1>
+    </section>
   )
 }
 
